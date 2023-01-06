f0132e441d33ccabb725abdb01ac299bcbe13ae7
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-01-06 15:16:04 +0300

    06.01.2022

--- src/test/java/util/ElementHelper.java
+++ src/test/java/util/ElementHelper.java
@@ -1,4 +1,22 @@
 package util;
 
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class ElementHelper {
+    AppiumDriver driver;
+    WebDriverWait wait;
+    Actions action;
+
+    public ElementHelper(AppiumDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        this.action = new Actions(driver);
+    }
 }
