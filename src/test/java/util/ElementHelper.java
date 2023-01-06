6828471c9a7aef9268624f4b4c59180a22d5bb0a
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-01-06 15:23:57 +0300

    06.01.2022

--- src/test/java/util/ElementHelper.java
+++ src/test/java/util/ElementHelper.java
@@ -1,10 +1,7 @@
 package util;
 
 import io.appium.java_client.AppiumDriver;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
