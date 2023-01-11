1666d070f18890d6f247dfc37a4d215dea8a64b6
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-01-11 15:52:43 +0300

    11.01.2022

--- src/test/java/util/ElementHelper.java
+++ src/test/java/util/ElementHelper.java
@@ -1,7 +1,10 @@
 package util;
 
 import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -11,9 +14,26 @@ public class ElementHelper {
     WebDriverWait wait;
     Actions action;
 
-    public ElementHelper(AppiumDriver driver) {
+    public ElementHelper(AppiumDriver driver){
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         this.action = new Actions(driver);
     }
+    public WebElement presenceElement(By key){
+        return
+                wait.until(ExpectedConditions.presenceOfElementLocated(key));
+    }
+    public WebElement findElement(By key){
+        WebElement element = presenceElement(key);
+        return element;
+    }
+    public void click(By key){
+        findElement(key).click();  // click --- tıklamayı sağlar.
+    }
+
+    public void sendKey(By key , String text){
+        findElement(key).sendKeys(text);   //sendKeys --- Text gondermeyi sağlar.
+    }
+
+
 }
